863d3338437f5af7570336fa6f3d52b2dca72aea
TheDokTor <orkv1234@gmail.com>
2023-04-16 23:53:17 +0300

    test

---
+++ dist/assets/index-4e91bc33.js
@@ -0,0 +1,1 @@
+import{t}from"./index-13a037a7.js";const a=t("<div><h2>Calculator");function o(){return a()}export{o as default};
---
+++ dist/assets/index-b65c84e2.js
@@ -0,0 +1,1 @@
+import{t}from"./index-13a037a7.js";const i=t("<div><h2>About</h2><p>This is a simple calculator app built with SolidJS.");function a(){return i()}export{a as default};
